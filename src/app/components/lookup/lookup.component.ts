8901eb60c260fd720c31bca9dcf8cdddfac0a8cf
SofCuev14 <sofia.bea.cuevas14@gmail.com>
2024-11-07 17:05:47 +0800

    lookup sample

--- src/app/components/lookup/lookup.component.ts
+++ src/app/components/lookup/lookup.component.ts
@@ -14,9 +14,10 @@ import { SohoAutoCompleteComponent, SohoAutoCompleteModule, SohoLookupComponent,
 export class LookupComponent {
   @ViewChild("sohoLookupComponent", { static: false })
   sohoLookupComponent?: SohoLookupComponent;
+  selectedEntityId: number = 0;
 
   ngOnChanges(changes: SimpleChanges): void {
-    (this.sohoLookupComponent as any).lookup.data= this.dataset
+    (this.sohoLookupComponent as any).lookup.data = this.dataset
     this.sohoLookupComponent?.updated();
     //this.sohoLookupComponent?.updateDataset(this.dataset, {});
   }
@@ -30,6 +31,7 @@ export class LookupComponent {
     label: "productName",
     value: "productId",
     template: `<script id="autocomplete-template-lookup" type="text/html">
+    <li id="{{listItemId}}" data-index="{{index}}" {{#hasValue}}data-value="{{value}}"{{/hasValue}} role="listitem">
       <a href="#" tabindex="-1">
         <span>{{{label}}}</span>
         <small>{{{value}}}</small>
@@ -44,13 +46,27 @@ export class LookupComponent {
       console.log("Autocomplete selected", e);
       console.log(e[2].index);
       console.log(this.dataset);
-      this.dataset.splice(e[2].index, 1);
+      this.selectedEntityId = e[2].id;
       console.log(this.dataset);
     },
     // beforeopen: (e: any) => {
     //   console.log("Autocomplete beforeopen", e);
     // },
   };
+  onButtonClick() {
+    console.log('BUTTON CLICK');
+    this.dataset = this.dataset.filter(item => item.id !== this.selectedEntityId);
+    let newDataset = this.dataset;
+    console.log('newDataset');
+    console.log(newDataset);
+    if (this.sohoLookupComponent?.dataset) {
+      this.sohoLookupComponent.dataset = newDataset;
+      this.sohoLookupComponent?.updated();
+      //this.sohoLookupComponent?.updateDataset(this.dataset, {});
+      console.log('this.sohoLookupComponent.dataset');
+      console.log(this.sohoLookupComponent.dataset);
+    }
+  }
 
   dataset: Order[] = [
     {
@@ -131,6 +147,87 @@ export class LookupComponent {
       action: "On Hold",
     },
   ];
+
+  dataset2: Order[] = [
+    {
+      id: 1,
+      productId: "1-1",
+      productName: "Ice Cream",
+      activity: "Assemble Paint",
+      quantity: 1,
+      price: 210.99,
+      status: "OK",
+      orderDate: new Date(2014, 12, 8),
+      action: "Action",
+    },
+    {
+      id: 2,
+      productId: "1-2",
+      productName: "Cookie",
+      activity: "Inspect and Repair",
+      quantity: 2,
+      price: 210.99,
+      status: "",
+      orderDate: new Date(2015, 7, 3),
+      action: "On Hold",
+    },
+    {
+      id: 3,
+      productId: "1-3",
+      productName: "Cupcake",
+      activity: "Inspect and Repair",
+      quantity: 1,
+      price: 120.99,
+      status: "",
+      orderDate: new Date(2014, 6, 3),
+      action: "Action",
+    },
+    {
+      id: 4,
+      productId: "1-4",
+      productName: "Pretzel",
+      activity: "Assemble Paint",
+      quantity: 3,
+      price: 210.99,
+      status: "OK",
+      orderDate: new Date(2015, 3, 3),
+      action: "Action",
+    },
+    {
+      id: 5,
+      productId: "1-5",
+      productName: "Smoothie",
+      activity: "Inspect and Repair",
+      quantity: 4,
+      price: 210.99,
+      status: "OK",
+      orderDate: new Date(2015, 5, 5),
+      action: "On Hold",
+    },
+    {
+      id: 6,
+      productId: "1-6",
+      productName: "Brownie",
+      activity: "Inspect and Repair",
+      quantity: 41,
+      price: 120.99,
+      status: "OK",
+      orderDate: new Date(2014, 6, 9),
+      action: "On Hold",
+    },
+    {
+      id: 7,
+      productId: "1-7",
+      productName: "Leche Flan",
+      activity: "inspect and Repair",
+      quantity: 41,
+      price: 123.99,
+      status: "OK",
+      orderDate: new Date(2014, 6, 9),
+      action: "On Hold",
+    },
+  ];
+  datasetTitle = 'Dataset'
 
 }
 
